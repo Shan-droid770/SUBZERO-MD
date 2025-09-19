39606f2c51f037790611fa06af5e7b28127790b1
Shan-droid770 <kumaradissanayaka30@gmail.com>
2025-09-19 12:54:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;yEF1ULrR#firC2jZIuS-IjYu5JMD_I2PyrG-LSK_HLiP8GhOJRkM", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94710927228", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
