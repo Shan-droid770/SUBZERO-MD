d31a9527bf97b186c42ef97a8efc34798dd20504
Shan-droid770 <kumaradissanayaka30@gmail.com>
2025-12-03 10:46:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "Ice~hYzY8x", // Put Session Id Here Inside "*"
+  SESSION_ID: "Ice~TNfhOa", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "94724898578", // Put Owner Number Here 
   PREFIX: ",", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
